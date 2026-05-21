01740733f495099283984f7727c53bde36068ae3
Álisson Pinheiro <alisson1017@gmail.com>
2026-05-20 23:25:20 -0300

    Upload original

---
+++ app.js
@@ -0,0 +1,35 @@
+//definir variavel "contagem" = 'A' + 0
+//definir variavel "input" com prompt
+//remover 'A' de contagem
+//adicionar input a contagem
+//iniciar loop while contagem for diferente de "A0"
+//    =0
+//        mostrar
+//        pedir novo input
+//        adicionar input a contagem
+//    <0
+//        mostrar
+//        adicionar 1
+//    >0
+//        mostrar
+//        subtrair 1
+//]
+let contagem = 'A0';
+let input = prompt('Digite um número para começar a contagem (pro)regressiva:');
+if (contagem == 'A0') {
+    contagem = parseInt(contagem) + input;
+}
+if (contagem != 'A0'){
+        if (contagem == 0) {
+            alert(contagem);
+            contagem = parseInt(prompt('Digite um número para começar uma nova contagem (pro)regressiva:'));
+        }
+    else if (contagem < 0) {
+        alert(contagem);
+        contagem++;
+    }
+    else if (contagem > 0) {
+        alert(contagem);
+        contagem--;
+    }
+}
